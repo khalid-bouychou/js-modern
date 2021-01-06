0bc9cef32a757d54eb5b10f2030809d1647ca27c
KHALID BOUYCHOU <41027645+khalid-bouychou@users.noreply.github.com>
2021-01-06 07:12:05 -0800

    Add files via upload

---
+++ Callback/exercice-callback.js
@@ -0,0 +1,31 @@
+function randomize(min , max ,callback){
+ 
+   return callback ( Math.ceil( 
+       
+     Math.random()*(min-max) +max 
+) )
+;
+}
+
+function multipfour( randomNumber ){
+ 
+   console.log(`Random number is ${randomNumber}`)
+
+    return randomNumber * 4
+
+}
+
+
+function divfive( randomNumber ){
+  
+  console.log(`Random number is ${randomNumber}`)
+
+    return randomNumber / 5
+
+}
+
+
+
+const res= randomize(1, 7 ,multipfour)
+
+console.log( `Resulte is : ${res}`)
